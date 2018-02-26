e1bf5530a592ec6a996494ff8ca611f5a7b14670
ltjs1024 <hujiasongbj@gmail.com>
2018-02-26 15:27:11 +0800

    V1.2 完善search(Guitar),返回多个匹配结果

--- src/com/stone/guitar/Inventory.java
+++ src/com/stone/guitar/Inventory.java
@@ -48,12 +48,13 @@ public class Inventory {
     }
 
     /**
-     * 搜索：接受客户的理想的吉他细节，从库存中匹配并返回吉他对象
+     * 搜索：接受客户的理想的吉他细节，从库存中匹配并返回所有符合客户规格的所有吉他
      *
      * @param searchGuitar 客户的理想的吉他细节
-     * @return 返回库存中匹配的吉他对象， 没有匹配到则返回null
+     * @return 返回符合客户规格的所有吉他
      */
-    public Guitar search(Guitar searchGuitar) {
+    public List<Guitar> search(Guitar searchGuitar) {
+        List<Guitar> result = new ArrayList<>();
         for (Guitar guitar : guitars) {
             if (guitar.getBuilder() != searchGuitar.getBuilder()) {
                 continue;
@@ -71,8 +72,8 @@ public class Inventory {
             if (guitar.getTopWood() != searchGuitar.getTopWood()) {
                 continue;
             }
-            return guitar;
+            result.add(guitar);
         }
-        return null;
+        return result;
     }
 }
--- test/com/stone/guitar/GuitarTest.java
+++ test/com/stone/guitar/GuitarTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.List;
+
 public class GuitarTest {
 
     private static Inventory guitars;
@@ -16,7 +18,8 @@ public class GuitarTest {
     }
 
     private static void initializeInventory(Inventory inventory) {
-
+        inventory.addGuitar("V3721", 1599.95, Builder.GIBSON,
+                "Gibson", Type.ACOUSTIC, Wood.BRAZILIAN_ROSEWOOD, Wood.BRAZILIAN_ROSEWOOD);
         inventory.addGuitar("V95693", 1499.95, Builder.FENDER,
                 "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
         inventory.addGuitar("V9512", 1549.95, Builder.FENDER,
@@ -29,26 +32,45 @@ public class GuitarTest {
     }
 
     /**
+     * 库存中匹配到唯一的吉他
+     *
      * @throws Exception
      */
     @Test
-    public void testSearch() throws Exception {
-        Guitar searchGuitar = new Guitar("", 0, Builder.FENDER,
-                "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
-        Guitar search = guitars.search(searchGuitar);
-        String expected = "Guitar{serialNumber='V95693', price=1499.95, builder='Fender', model='Stratocastor', type='Electric', backWood='Alder', topWood='Alder'}";
-        Assert.assertEquals(expected, search.toString());
+    public void testSearchOnly() throws Exception {
+        Guitar searchGuitar = new Guitar("", 0, Builder.GIBSON,
+                "Gibson", Type.ACOUSTIC, Wood.BRAZILIAN_ROSEWOOD, Wood.BRAZILIAN_ROSEWOOD);
+        List<Guitar> searchGuitars = guitars.search(searchGuitar);
+        String expected = "[Guitar{serialNumber='V3721', price=1599.95, builder='Gibson', model='Gibson', type='Acoustic', backWood='Brazilian Rosewood', topWood='Brazilian Rosewood'}]";
+        Assert.assertEquals(expected, searchGuitars.toString());
     }
 
     /**
+     * 库存中匹配到多个吉他
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSearchMultis() throws Exception {
+        Guitar searchGuitar = new Guitar("", 0, Builder.FENDER,
+                "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+        List<Guitar> result = guitars.search(searchGuitar);
+        String expected = "[Guitar{serialNumber='V95693', price=1499.95, builder='Fender', model='Stratocastor', type='Electric', backWood='Alder', topWood='Alder'}," +
+                " Guitar{serialNumber='V9512', price=1549.95, builder='Fender', model='Stratocastor', type='Electric', backWood='Alder', topWood='Alder'}]";
+        Assert.assertEquals(expected, result.toString());
+    }
+
+    /**
+     * 库存中没有匹配到
+     *
      * @throws Exception
      */
     @Test
     public void testSearchNotExist() throws Exception {
         Guitar searchGuitar = new Guitar("", 0, Builder.ANY,
                 "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
-        Guitar search = guitars.search(searchGuitar);
-        String expected = null;
-        Assert.assertEquals(expected, search == null ? null : search.toString());
+        List<Guitar> search = guitars.search(searchGuitar);
+        String expected = "[]";
+        Assert.assertEquals(expected, search.toString());
     }
 }
