8fc89ac46bff650af88e80ae5e6faa7bcbc203db
ltjs1024 <hujiasongbj@gmail.com>
2018-03-02 11:44:30 +0800

    DogV1.2 自动关门,被关在门外的处理

--- src/com/stone/dog/DogDoorSimulator.java
+++ src/com/stone/dog/DogDoorSimulator.java
@@ -15,7 +15,19 @@ public class DogDoorSimulator {
 
         System.out.println("\nFido has gone outside...");
         System.out.println("\nFido's all done...");
+
+        try {
+            Thread.currentThread().sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("...but he's stuck outside!");
+        System.out.println("\nFido starts barking...");
+        System.out.println("...so Gina grabs the remote control.");
+        remote.pressButton();
         System.out.println("\nFido's back inside...");
+
 
     }
 
